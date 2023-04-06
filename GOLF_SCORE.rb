832c131cf1db1a685a24d0c941fd5b87f7a9d989
CMaldonado13 <christian.maldonado1@kellogg.northwestern.edu>
2023-04-06 17:41:13 +0000

    pulling data

--- GOLF_SCORE.rb
+++ GOLF_SCORE.rb
@@ -2,6 +2,34 @@ require 'nokogiri'
 require 'net/http'
 require 'uri'
 
+
 url = URI.parse('https://www.espn.com/golf/leaderboard')
 response = Net::HTTP.get_response(url)
 doc = Nokogiri::HTML(response.body)
+# Locate the table element that contains the data
+table = doc.css('table').first
+
+# Extract the rows and cells from the table element
+rows = table.css('tr')
+rows.shift # Remove the header row if present
+
+# Iterate through the rows and extract the data from the "PLAYER" column
+players = []
+rows.each do |row|
+  cols = row.css('td')
+  player_link = cols[2].css('a.leaderboard_player_name').first
+  if player_link
+    player_name = player_link.text.strip
+    score = cols[3].text.strip
+    today = cols[4].text.strip
+    thru = cols[5].text.strip
+    players << "#{player_name}: #{score} (#{today}) [#{thru}]"
+  end
+end
+
+# Write the list of player names and scores to a file
+File.open("output.txt", "w") do |file|
+  players.each do |player|
+    file.puts(player)
+  end
+end
