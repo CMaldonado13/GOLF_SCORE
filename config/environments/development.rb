43aa1aa0c35753ec38a82008f2a7979d5afaa454
CMaldonado13 <christian.maldonado1@kellogg.northwestern.edu>
2023-04-06 18:12:14 +0000

    geting there

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,8 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.hosts << "3000-cmaldonado13-golfscore-b08w49ui0sc.ws-us93.gitpod.io"
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
